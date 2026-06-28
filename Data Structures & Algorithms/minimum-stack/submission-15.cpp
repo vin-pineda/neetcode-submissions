8e331159ec1dc9ffb234586f9209c65f55ec2e5c
Vincent Pineda <112200262+vin-pineda@users.noreply.github.com>
2026-06-27 17:07:24 -0700

    Add: minimum-stack - submission-15

---
+++ Data Structures & Algorithms/minimum-stack/submission-15.cpp
@@ -0,0 +1,26 @@
+class MinStack {
+public:
+    stack<int> minstack;
+    stack<int> min;
+    
+    MinStack() {}
+    
+    void push(int val) {
+        minstack.push(val);
+        if (min.empty() || val <= min.top()) min.push(val);
+    }
+    
+    void pop() {
+        int x = minstack.top();
+        minstack.pop();
+        if (x == min.top()) min.pop();
+    }
+    
+    int top() {
+        return minstack.top();
+    }
+    
+    int getMin() {
+        return min.top();
+    }
+};
