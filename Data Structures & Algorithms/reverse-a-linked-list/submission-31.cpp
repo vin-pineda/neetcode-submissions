3a85a8dfb22082fe81c307b2a764a28da4029453
Vincent Pineda <112200262+vin-pineda@users.noreply.github.com>
2026-06-30 13:29:16 -0700

    Add: reverse-a-linked-list - submission-31

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-31.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* new_head = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;    
+        return new_head;    
+    }
+};
