88e38d2d2cf98f7a053ae847ce6509198e1b48de
Vincent Pineda <112200262+vin-pineda@users.noreply.github.com>
2026-07-04 20:16:58 -0700

    Add: insertionSort - submission-3

---
+++ Data Structures & Algorithms/insertionSort/submission-3.cpp
@@ -0,0 +1,24 @@
+// Definition for a Pair
+// class Pair {
+// public:
+//     int key;
+//     string value;
+//
+//     Pair(int key, string value) : key(key), value(value) {}
+// };
+class Solution {
+public:
+    vector<vector<Pair>> insertionSort(vector<Pair>& pairs) {
+        vector<vector<Pair>> res;
+        if (pairs.empty()) return res;
+
+        for (int i = 0; i < pairs.size(); i++) {
+            int j = i - 1;
+            while (j >= 0 && pairs[j].key > pairs[j + 1].key) {
+                swap(pairs[j], pairs[j + 1]); j--;
+            }
+            res.push_back(pairs);
+        }
+        return res;
+    }
+};
